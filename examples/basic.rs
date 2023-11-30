13fa028ed18dd084c4462af0c0328cf3876224ed
Nithin Muthukumar <kathrikai30201094@gmail.com>
2023-11-29 23:30:28 -0500

    adding debug

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,9 +1,13 @@
 use shrs::prelude::*;
+use shrs_mux::MuxPlugin;
 use shrs_ollama::OllamaPlugin;
+use shrs_output_capture::OutputCapturePlugin;
 
 fn main() {
     let myshell = ShellBuilder::default()
         .with_plugin(OllamaPlugin::new())
+        .with_plugin(OutputCapturePlugin)
+        .with_plugin(MuxPlugin::new())
         .build()
         .unwrap();
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use request::OllamaClient;
 use reqwest::Client;
 use shrs::{
     anyhow::{self, Result},
-    prelude::{AfterCommandCtx, BuiltinCmd, CmdOutput, Context, Plugin, Runtime, Shell},
+    prelude::{AfterCommandCtx, BuiltinCmd, CmdOutput, Context, Plugin, Prompt, Runtime, Shell},
 };
-use shrs_output_capture::OutputCapturePlugin;
+use shrs_output_capture::{OutputCapturePlugin, OutputCaptureState};
 mod request;
 
 pub struct OllamaPlugin {}
@@ -47,13 +47,13 @@ impl OllamaState {
 struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
-
-    prompt: Option<Vec<String>>,
 }
 #[derive(Debug, Subcommand)]
 enum Commands {
     Reset,
     Model { model: String },
+    Prompt { prompt: Vec<String> },
+    Debug,
 }
 
 pub struct OllamaBuiltin {}
@@ -66,14 +66,40 @@ impl BuiltinCmd for OllamaBuiltin {
         args: &Vec<String>,
     ) -> anyhow::Result<CmdOutput> {
         let cli = Cli::try_parse_from(args)?;
-        if let Some(prompt) = cli.prompt {
-            if let Some(state) = ctx.state.get_mut::<OllamaState>() {
-                let response = state.client.generate(
-                    prompt.join(" "),
-                    state.model.clone(),
-                    state.context.clone(),
-                )?;
-                ctx.out.println(response.response)?;
+
+        let last_output = if let Some(output_capture) = ctx.state.get::<OutputCaptureState>() {
+            Some(output_capture.last_output.clone())
+        } else {
+            None
+        };
+
+        if let Some(state) = ctx.state.get_mut::<OllamaState>() {
+            if let Some(command) = cli.command {
+                match command {
+                    Commands::Model { model } => (),
+                    Commands::Prompt { prompt } => {
+                        let response = state.client.generate(
+                            prompt.join(" "),
+                            state.model.clone(),
+                            state.context.clone(),
+                        )?;
+                        ctx.out.println(response.response)?;
+                    }
+                    Commands::Reset => {
+                        state.context.clear();
+                    }
+                    Commands::Debug => {
+                        if let Some(lo) = last_output {
+                            println!("{:?}", lo);
+                            let response = state.client.generate(
+                                format!("Debug this {:?}", lo.stderr.clone()),
+                                state.model.clone(),
+                                state.context.clone(),
+                            )?;
+                            ctx.out.println(response.response)?;
+                        }
+                    }
+                }
             }
         }
 
